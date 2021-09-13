24650676f1b0f5f5c83676c1298b9eb337ff9430
Antonio <ditacj78@gmail.com>
2021-09-13 19:36:40 +0200

    App.js : added state and Button

--- rps-rn/App.js
+++ rps-rn/App.js
@@ -1,14 +1,26 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import Player from "./Player";
 
 const weapons = ["rock", "paper", "scissors"];
 export default function App() {
+
+  let state = {
+    playerOne: weapons[0],
+    playerTwo: weapons[0],
+    winner: ""
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Rock, Paper, Scissors</Text>
       <View>
         <Player/>
+      </View>
+      <View>
+        <TouchableOpacity>
+          <Text>Button</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
