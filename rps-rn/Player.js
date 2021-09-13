9cfc83db19e65ca5fc19a306fde9841a726a8fd5
Antonio <ditacj78@gmail.com>
2021-09-13 19:27:32 +0200

    init Player.js

---
+++ rps-rn/Player.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Player = ({weapons}) => {
+    return (
+        <div className="p">
+
+        </div>
+    )
+}
+
+export default Player;
