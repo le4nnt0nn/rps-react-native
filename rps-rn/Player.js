aeb2b5b1e19d0f7bfd39eb3061ca5216b011414c
Antonio <ditacj78@gmail.com>
2021-09-13 20:11:46 +0200

    Player.js : added style for player icon and imported assets

--- rps-rn/Player.js
+++ rps-rn/Player.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import rock from "./assets/icons/rock.png";
+import paper from "./assets/icons/paper.png";
+import scissors from "./assets/icons/scissors.png";
 
-const Player = ({weapons}) => {
-    return (
-        <View className="p">
-            <Text>ex</Text>
-        </View>
-    )
-}
+const Player = ({ weapons }) => {
+  return (
+    <View>
+      <Image style={styles.playerIcon} source={rock} />
+    </View>
+  );
+};
+
+// styles
+const styles = StyleSheet.create({
+    playerIcon: {
+        width: 100,
+        height: 100,
+        margin: 15
+    }
+});
 
 export default Player;
