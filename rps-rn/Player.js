6517b6ffa0296392cd5ed43d5726284c9fef9c64
Antonio <ditacj78@gmail.com>
2021-09-17 12:11:55 +0200

    Player.js : added comment and formated with prettier

--- rps-rn/Player.js
+++ rps-rn/Player.js
@@ -7,20 +7,24 @@ import scissors from "./assets/icons/scissors.png";
 const Player = ({ weapon }) => {
   return (
     <View>
-      <Image style={styles.playerIcon} source={
+      {/* Image source with props */}
+      <Image
+        style={styles.playerIcon}
+        source={
           weapon === "rock" ? rock : weapon === "scissors" ? scissors : paper
-      } />
+        }
+      />
     </View>
   );
 };
 
 // styles
 const styles = StyleSheet.create({
-    playerIcon: {
-        width: 200,
-        height: 200,
-        margin: 15
-    }
+  playerIcon: {
+    width: 200,
+    height: 200,
+    margin: 15,
+  },
 });
 
 export default Player;
